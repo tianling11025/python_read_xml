1c3705e256d24ad35b577e7003c6c108cd229c7c
yixin <735668637@qq.com>
2016-08-10 18:15:07 +0800

    1st

--- readxml.py
+++ readxml.py
@@ -3,7 +3,7 @@ import xml.etree.ElementTree as ET
 import os
 
 def readxml():
-
+    object_name = "2009081097"
     fp = os.path.abspath("")
     print fp
 
@@ -13,15 +13,20 @@ def readxml():
     print root[1][3].tag
     print root[1][3].attrib
     print root[1][3].text
-
+    child = root.find("student")
+    no = child.get("no")
+    print no
 
     for student in root:
         print student.tag,student.attrib
 
     for student in root.findall("student"):
         no = student.get("no")
-        name = student.find("name").text
-        print no,name
+        if no == object_name:
+            name = student.find("name").text
+            age = student.find("age").text
+            gender = student.find("gender").text
+            print no,name,age,gender
 
 
 
