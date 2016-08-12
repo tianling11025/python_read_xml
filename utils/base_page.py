79f45bee1702919467ae106971f0edd6e77f4720
yixin <735668637@qq.com>
2016-08-12 15:47:28 +0800

    1st

--- utils/base_page.py
+++ utils/base_page.py
@@ -3,26 +3,33 @@ import os
 import xml.etree.ElementTree as ET
 import appium.webdriver.common.mobileby as By
 
-filepath = os.path.abspath("")+"/pages/work_page.xml"
+filepath = os.path.abspath("..")+"/pages/work_page.xml"
 
 class BasePage(object):
 
-    def __init__(self,driver):
-        """构造函数"""
-        self._driver = driver
+    def read_element(self,page_name,object_name):
 
-    def read_element(self):
-        tree = ET.parse(filepath)
-        for element in tree.findall("page"):
-            name = element.get("name")
-            if name == u"工作":
-                value = element.attrib
-                print value['value']
+        pages = ET.parse(filepath)
+        for page in pages.findall("page"):
+            # 第一次遍历,获取所有page页
+            pagename = page.get("pagename")
+            if pagename == page_name:
+                # 获取所需页面
+                for object in page.findall("object"):
+                    # 第二次遍历,获取该页面下所有object对象
+                    name = object.get("name")
+                    if name == object_name:
+                        # 获取所需元素
+                        value = object.attrib
+                        print value['findby'], value['value']  # 输出定位方式及值
 
-    def find_element(self):
+
+    def find_element(self,findby,value):
+
+
         pass
 
 
 if __name__ == "__main__":
-    BasePage.read_element()
-
+    test = BasePage()
+    test.read_element(u'工作',u'考勤')
